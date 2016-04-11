c708a86da8bb53adc0c9293da90aa35ff0fb176b
xraymonx <raymon_jesse2@hotmail.com>
2016-04-11 11:58:18 +0200

    InputScript and Tags

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Nothing2 : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
