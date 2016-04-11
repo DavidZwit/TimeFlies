4af3c0d635e7c14222d87526a83f95b38aa5005e
xraymonx <raymon_jesse2@hotmail.com>
2016-04-11 14:17:05 +0200

    player

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour {
+
+	PlayerMovement playerMovement;
+
+	// Use this for initialization
+	void Start () {
+
+
+		playerMovement = GetComponent<PlayerMovement> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A)) {
+		playerMovement.moveNegative = true;
+		} else {
+		playerMovement.moveNegative = false;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			playerMovement.movePositive = true;
+		} else {
+			playerMovement.movePositive = false;
+		}
+		if (Input.GetKey (KeyCode.W)) {
+			Debug.Log ("Ik ga kenker snel");
+			playerMovement.ChangeTime (4f);
+		} 
+		if (Input.GetKey (KeyCode.S)) {
+			playerMovement.ChangeTime (0.4f);
+		} 
+		if (Input.GetKey (KeyCode.R)) 
+		{
+			playerMovement.ChangeTime (1f);
+		}
+	}
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,14 +2,45 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
+	
+	private GameObject _Player;
+	[SerializeField] private float movementSpeed = 20f;
+	private Vector3 _PlayerPos;
 
-	// Use this for initialization
+	private bool _movePositive = false;
+	public bool movePositive
+	{
+		set {_movePositive = value; }
+	}
+
+	private bool _moveNegative = false;
+	public bool moveNegative
+	{
+		set {_moveNegative = value; }
+	}
+		
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
 	
+		if (_moveNegative) {
+			_PlayerPos.x -= movementSpeed * Time.deltaTime; 
+		}
+		if (_movePositive) 
+		{
+			_PlayerPos.x += movementSpeed * Time.deltaTime; 
+		}
+
+
+
+		transform.position = new Vector3 (Mathf.Clamp (_PlayerPos.x, -25f, 25f), 5f, 0);
+	}
+
+	public void ChangeTime(float timeSet)
+	{
+		Time.timeScale = timeSet;
 	}
 }
