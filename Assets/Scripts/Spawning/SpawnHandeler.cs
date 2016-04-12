f2aad1b4f787ed62b8af83c91dc7a31141e4c11e
DavidZwit <davidzwitser@gmail.com>
2016-04-12 11:45:09 +0200

    Fixing the spawning

--- Assets/Scripts/Spawning/SpawnHandeler.cs
+++ Assets/Scripts/Spawning/SpawnHandeler.cs
@@ -9,7 +9,7 @@ public class SpawnHandeler : MonoBehaviour {
     List<GameObject> ActiveObjects = new List<GameObject>();
 
     [SerializeField]
-    float SpawnRate = 0.01f, moveSpeed = 1, mapWidth = 50, deletePos = 0, spawnAmound = 10;
+    float SpawnRate, moveSpeed = 1, mapWidth = 50, deletePos = 0, spawnAmound = 10;
 
     [SerializeField]
     Vector3 StartPosition = new Vector3(0, 0, 1000);
@@ -17,31 +17,44 @@ public class SpawnHandeler : MonoBehaviour {
     [SerializeField]
     Transform playerPosition;
 
-
-    void FixedUpdate()
+    void Start()
     {
-        if (Time.time % SpawnRate == 0) {
+        StartCoroutine("spawnObjects");
+    }
+
+    IEnumerator spawnObjects()
+    {
+        while (playerPosition != null)
+        {
             for (int i = 0; i < spawnAmound; i++)
             {
                 float randomX = Random.Range(-(mapWidth / 2), (mapWidth / 2)) + playerPosition.position.x;
                 int random = (int)Random.Range(0, SpawnObjects.Length);
-                print(StartPosition.z);
+
                 ActiveObjects.Add(Instantiate(SpawnObjects[random],
                     new Vector3(randomX, StartPosition.y, StartPosition.z),
                     new Quaternion(0, 0, 0, 0)) as GameObject);
+                print("spawnObjects");
             }
-        }
+            yield return new WaitForSeconds(0.1f);
 
-        for (var i = 0; i < ActiveObjects.Count; i++) {
+        }
+    }
+
+    void FixedUpdate()
+    {
+        for (var i = 0; i < ActiveObjects.Count; i++)
+        {
             ActiveObjects[i].transform.Translate(Vector3.forward * -moveSpeed);
             Vector3 activeObjectPosition = ActiveObjects[i].transform.position;
             Vector2 SpawnBoundries = new Vector2(
-                playerPosition.position.x - (mapWidth / 2), 
+                playerPosition.position.x - (mapWidth / 2),
                 playerPosition.position.x + (mapWidth / 2));
 
-            if (activeObjectPosition.z < deletePos || 
-                activeObjectPosition.x < SpawnBoundries.x || 
-                activeObjectPosition.x > SpawnBoundries.y) {
+            if (activeObjectPosition.z < deletePos ||
+                activeObjectPosition.x < SpawnBoundries.x ||
+                activeObjectPosition.x > SpawnBoundries.y)
+            {
 
                 Destroy(ActiveObjects[i]);
                 ActiveObjects.Remove(ActiveObjects[i]);
