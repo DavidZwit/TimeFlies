c94f3576f19b21bfbd97c31beaee69835dc1c4bc
xraymonx <raymon_jesse2@hotmail.com>
2016-04-13 11:36:16 +0200

    rotation!

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+	private float _rotation;
+	private Quaternion _GameRotation;
+	private GameObject _parent;
+	// Use this for initialization
+	void Start () {
+		_GameRotation = this.transform.rotation;
+		_rotation = _GameRotation.y;
+		_parent = GameObject.FindGameObjectWithTag ("heli");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_rotation = 720;
+		transform.Rotate (new Vector3 (0, _rotation,0)*Time.deltaTime);
+	}
+}
